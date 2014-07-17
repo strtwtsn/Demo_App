22f3e09c4f0e180b322a242bd3c661263b202b0a
Stuart Watson <stuart.watson@unep-wcmc.org>
2014-07-17 11:02:00 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'dd5f16eefe0e587cafe06095a8cdf7749f0b57543fd12cf0754bd9eabb42e1f08dc4d957bda4a90d3c85e4d26a7ded8244dcbbf4ecd669fbc830b1eb35830086'
